872e48d040bee52f38a34891d5ce63ade261c73d
Mohammad Ali Bazzazi <81759694+bazzazi@users.noreply.github.com>
2021-12-23 21:17:55 +0330

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import pyttsx3
+
+def speech(word):
+    engine = pyttsx3.init()
+    engine.setProperty('rate' , 120)
+    voices = engine.getProperty('voices')
+    engine.setProperty('voice', voices[0].id)
+    engine.say(word)
+    engine.runAndWait()
+
+word = input('Enter a Phrase: ')
+speech(word)
